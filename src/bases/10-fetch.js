1de67e4fc2306efac120e773642f3260f011e613
David Zapata <dzr794@gmail.com>
2024-02-20 17:01:36 -0500

    fin del reforzamiento de JS

--- src/bases/10-fetch.js
+++ src/bases/10-fetch.js
@@ -6,13 +6,13 @@ const peticion = fetch(fetchUrl);
 // console.log(fetchUrl);
 
 peticion
-.then(resp => resp.json())
-.then(({ data }) => {
-  const { url } = data.images.original;
-  const img = document.createElement('img');
-  img.src = url;
+  .then(resp => resp.json())
+  .then(({ data }) => {
+    const { url } = data.images.original;
+    const img = document.createElement('img');
+    img.src = url;
 
-  document.body.append(img);
-}).catch(console.warn)
-  
+    document.body.append(img);
+  }).catch(console.warn)
+
 
