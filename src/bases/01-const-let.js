56c3590afe80ffbc26666129bf3ec4e7a3220469
David Zapata <dzr794@gmail.com>
2024-02-20 14:42:25 -0500

    se hablaron de variables y const y no usar mas var

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+// variables y constantes
+
+const nombre = 'David';
+const apellido = 'Zapata';
+
+let valorDado = 6;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  const nombre = 'G';
+  const valorDado =  2;
+
+  console.log(nombre, valorDado);
+}
+
+console.log(valorDado);
